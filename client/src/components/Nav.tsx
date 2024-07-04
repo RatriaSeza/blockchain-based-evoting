200688d44b756c591bcec0373b99491789f1fc8b
RatriaSeza <satriareza2002@gmail.com>
2024-07-04 22:43:59 +0700

    feat: nav styling and install hero icon

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -1,15 +1,37 @@
-import React from "react";
+import { HomeIcon as HomeOutline, DocumentCheckIcon as VoteOutline, UserIcon as ProfileOutline } from '@heroicons/react/24/outline';
+import { HomeIcon as HomeSolid, DocumentCheckIcon as VoteSolid, UserIcon as ProfileSolid } from '@heroicons/react/24/solid';
+import React from 'react';
 
-const Nav = () => {
+type NavItemProps = {
+  isActive: boolean;
+  icon: React.ElementType; 
+  link: string;
+  text: string;
+};
+
+type NavProps = {
+  active: string;
+}
+
+const NavItem: React.FC<NavItemProps> = ({ isActive, icon: Icon, link, text }) => {
+  return (
+    <li className={`flex flex-col justify-center items-center gap-1 ${isActive ? 'text-black' : 'text-gray-500'}`}>
+      <Icon className="size-7" />
+      <a href={link} className={`${isActive ? 'font-medium' : 'font-normal'}`}>{text}</a>
+    </li>
+  )
+};
+
+const Nav: React.FC<NavProps> = ({ active }) => {
   return (
     <nav className="fixed bottom-0 w-full ">
-      <div className="container w-11/12 mx-auto flex justify-between items-center py-4 bg-gray-700">
-          <a href="/login" className="text-xl font-bold">Home</a>
-          <a href="/register" className="text-xl font-bold">Vote</a>
-          <a href="/" className="text-xl font-bold">Profile</a>
-      </div>
+      <ul className="container w-full flex justify-around items-center py-5 bg-white text-base font-normal rounded-t-[50px]">
+        <NavItem isActive={active === 'home'} icon={active === 'home' ? HomeSolid : HomeOutline} link="/" text="Home" />
+        <NavItem isActive={active === 'vote'} icon={active === 'vote' ? VoteSolid : VoteOutline} link="/vote" text="Vote" />
+        <NavItem isActive={active === 'profile'} icon={active === 'profile' ? ProfileSolid : ProfileOutline} link="/profile" text="Profile" />
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
 export default Nav;
--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -1,13 +1,17 @@
-import React from "react";
 import Nav from "../components/Nav";
 
 const Home = () => {
   return (
     <div className="bg-[#F6F7F9]">
-      <div className="container mx-auto h-svh w-svw">
-        sdf
+      <div className="container h-svh w-svw mx-6 my-4">
+        <div>
+          <p className="text-base leading-4">Hi, <br/><span className="text-xl font-bold">Satria Reza Ramadhan</span></p>
+          <div>
+            
+          </div>
+        </div>
       </div>
-        <Nav />
+      <Nav active="home" />
     </div>
   );
 };
