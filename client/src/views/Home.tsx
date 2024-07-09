67f1d6477c41305f6ff54784da499baf16f17292
RatriaSeza <satriareza2002@gmail.com>
2024-07-09 22:17:27 +0700

    chore: Add fontawesome script and implement countdown timer on Home view

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -1,39 +1,69 @@
+import { useState, useEffect } from "react";
 import Nav from "../components/Nav";
-import TimeIllustration from "../assets/img/time-illustration.svg";
 
 const Home = () => {
+  const [time, setTime] = useState({
+    hours: 10,
+    minutes: 0,
+    seconds: 10,
+  });
+
+  useEffect(() => {
+    const countdown = setInterval(() => {
+      setTime((prevTime) => {
+        let { hours, minutes, seconds } = prevTime;
+
+        if (seconds > 0) {
+          seconds--;
+        } else if (minutes > 0) {
+          seconds = 59;
+          minutes--;
+        } else if (hours > 0) {
+          seconds = 59;
+          minutes = 59;
+          hours--;
+        } else {
+          clearInterval(countdown);
+        }
+
+        return { hours, minutes, seconds };
+      });
+    }, 1000);
+
+    return () => clearInterval(countdown);
+  }, []);
+
+
   return (
     <div className="bg-[#111111] text-white">
       <div className="px-6 py-4">
         <div className="">
           <div>
-            <p className="text-base leading-4">
+            <p className="text-base leading-4 mb-6">
               Hi, <br />
               <span className="text-xl font-bold">Satria Reza Ramadhan</span>
             </p>
-            {/* <div className="mt-6 rounded bg-[#212121] h-80 shadow-card-sm">
-              Time Remaining
-            </div> */}
-            <div className="flex justify-between items-center gap-4 mt-6 px-6 py-4 rounded shadow-countdown bg-countdown">
-              <div className="flex flex-col justify-between gap-3">
-                <h4 className="font-medium">Time Remaining</h4>
-                <div className="text-black flex justify-betwee items-center">
-                  <div className="bg-gray-100 text-6xl rounded">
-                    <h2>10</h2>
+            <div className="relative flex justify-between items-center gap-4 px-6 py-4 rounded shadow-countdown bg-countdown">
+              <div className="flex flex-col justify-between gap-1">
+                <h4 className="font-medium grid items-center">Time Remaining</h4>
+                <div className="text-black grid grid-cols-11 gap-1">
+                  <div className="col-span-3 flex flex-col items-center bg-gray-100 font-semibold px-2 py-2 rounded">
+                    <span className="text-4xl">{String(time.hours).padStart(2, "0")}</span>
+                    <span className="text-xs">Hours</span>
                   </div>
-                  <div className="h-fit text-6xl">
-                    <h4>:</h4>
+                  <span className="text-5xl self-center text-center text-gray-100">:</span>
+                  <div className="col-span-3 flex flex-col items-center bg-gray-100 font-semibold px-2 py-2 rounded">
+                    <span className="text-4xl">{String(time.minutes).padStart(2, "0")}</span>
+                    <span className="text-xs">Minutes</span>
                   </div>
-                  <div className="bg-gray-100 text-6xl rounded">10</div>
-                  <span className="text-6xl">:</span>
-                  <div className="bg-gray-100 text-6xl rounded">10</div>
+                  <span className="text-5xl self-center text-center text-gray-100">:</span>
+                  <div className="col-span-3 flex flex-col items-center bg-gray-100 font-semibold px-2 py-2 rounded">
+                    <span className="text-4xl">{String(time.seconds).padStart(2, "0")}</span>
+                    <span className="text-xs">Seconds</span>
+                  </div>
                 </div>
               </div>
-              <img
-                className="min-w-16"
-                src={TimeIllustration}
-                alt="Time Illustration"
-              />
+              <span className="text-2xl absolute top-3 right-4 animate-pulse"><i className="fa-solid fa-hourglass-end"></i></span>
             </div>
           </div>
         </div>
