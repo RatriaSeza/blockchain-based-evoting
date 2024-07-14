84466665569e44a010d35a1457115985bc9e1838
RatriaSeza <satriareza2002@gmail.com>
2024-07-15 00:03:01 +0700

    chore: Update Button component to accept customClass prop

--- client/src/components/Vote/CandidateCard.tsx
+++ client/src/components/Vote/CandidateCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from '../Button'
 
 type CandidateCardProps = {
   candidate: {
@@ -9,14 +10,37 @@ type CandidateCardProps = {
     candidateNumber: number;
     chiefMajor: string;
     viceMajor: string;
+    chiefClassOf: number;
+    viceClassOf: number;
   };
 };
 
 const CandidateCard: React.FC<CandidateCardProps> = ({ candidate }) => {
   return (
-    <div className="bg-dark-card">
-      <img src={candidate.candidateImage} alt="" />
-      <button>Vote</button>
+    <div className="bg-dark-card shadow-inner shadow-neutral-800 rounded-xl">
+      <img className='w-full h-60 object-cover rounded-t-xl' src={candidate.candidateImage} alt={`Candidates ${candidate.candidateNumber} image`} />
+      <div className='px-4 py-4 rounded-t-xl'>
+        <div className='mb-2'>
+          <div className='flex justify-between items-center'>
+            <div>
+              <div className='mb-2'>
+                <p className='text-base font-semibold leading-4 '>{candidate.chiefName}</p>
+                <p className='text-xs font-light'>{candidate.chiefMajor} ({candidate.chiefClassOf})</p>
+              </div>
+              <div>
+                <p className='text-base font-semibold leading-4'>{candidate.viceName}</p>
+                <p className='text-xs font-light'>{candidate.viceMajor} ({candidate.viceClassOf})</p>
+              </div>
+            </div>
+            <p className='text-6xl font-semibold mr-2'>
+              {candidate.candidateNumber}
+            </p>
+          </div>
+        </div>
+        <div className='flex justify-center'>
+          <Button label='Vote' customClass='py-2 px-8' />
+        </div>
+      </div>
     </div>
   );
 }
--- client/src/views/Vote.tsx
+++ client/src/views/Vote.tsx
@@ -1,14 +1,30 @@
 import Nav from "../components/Nav";
 import CandidateCard from "../components/Vote/CandidateCard";
+import FirstCandidateImage from "../assets/img/candidates/first-candidate.jpg";
+import SecondCandidateImage from "../assets/img/candidates/second-candidate.jpg";
 
 const firstCandidateData = {
   id: 1,
   chiefName: "Satria Reza Ramadhan",
   viceName: "Dimas Maulana",
-  candidateImage: "../assets/img/candidates/first-candidate.jpg",
+  candidateImage: FirstCandidateImage,
   candidateNumber: 1,
-  chiefMajor: "Computer Science",
-  viceMajor: "Computer Engineering",
+  chiefMajor: "Informatika",
+  viceMajor: "Biologi",
+  chiefClassOf: 2021,
+  viceClassOf: 2021,
+};
+
+const secondCandidateData = {
+  id: 2,
+  chiefName: "Donald Trump",
+  viceName: "Barack Obama",
+  candidateImage: SecondCandidateImage,
+  candidateNumber: 2,
+  chiefMajor: "Kimia",
+  viceMajor: "Fisika",
+  chiefClassOf: 2021,
+  viceClassOf: 2021,
 };
 
 const Vote = () => {
@@ -18,13 +34,13 @@ const Vote = () => {
       <div className="px-6 py-4">
         <div className="mb-24">
           <div>
-            <p className="text-base leading-4 mb-6">
-              Hi, <br />
-              <span className="text-xl font-bold">Satria Reza Ramadhan</span>
-            </p>
+            <h2 className="mt-3 mb-6 text-center text-xl font-bold">
+              Calon Ketua BEM FSM Undip
+            </h2>
             <div className="mb-6">
-              <div>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                 <CandidateCard candidate={firstCandidateData} />
+                <CandidateCard candidate={secondCandidateData} />
               </div>
             </div>
           </div>
