4bb7e744d7d5cef474e47583d42bf425b901abd8
ratriaseza <satriareza2002@gmail.com>
2024-07-17 17:02:16 +0700

    chore: Update Input component to include value, icon, and onChange props

--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -14,16 +14,17 @@ const Input: React.FC<InputProps> = ({ label, value, type, placeholder, icon, on
   return (
     <div>
       <label className="text-neutral-300 font-medium">{label}</label>
-      <div className="relative">
+      <div className="relative mt-1">
         <input
-          className="bg-neutral-900 px-4 py-3 outline-none w-full text-white rounded-lg border-2 transition-colors duration-100 border-solid focus:border-[#596A95] border-[#2B3040] pr-10" // Adjust padding to make space for the icon
+          className="bg-neutral-900 px-4 py-3 outline-none w-full text-white rounded-lg border-2 transition-colors duration-100 border-solid focus:border-[#596A95] border-[#2B3040] pr-10"
           placeholder={placeholder}
           value={value}
           type={type}
+          onChange={onChange}
           {...props}
         />
         {icon && (
-          <button  className="absolute right-0 top-0 w-10 h-full flex justify-center items-center pr-3 text-[#596A95]" >
+          <button  className="absolute right-0 top-0 w-10 h-full flex justify-center items-center pr-3 text-[rgb(85_217_198)] hover:text-[rgb(53,142,129)] active:text-[rgb(85_217_198)]" onClick={onClickIcon} >
             {icon}
           </button  >
         )}
--- client/src/views/Login.tsx
+++ client/src/views/Login.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Input from "../components/Input";
+import Button from "../components/Button";
 
 const Login = () => {
   const [password, setPassword] = useState("");
@@ -7,23 +8,37 @@ const Login = () => {
 
   const handlePasswordVisibility = () => {
     setPasswordVisible(!passwordVisible);
-  }
+  };
 
   return (
     <div className="min-h-dvh text-neutral-100">
       <div className="h-dvh flex justify-center items-center">
-        <div className="w-11/12 md:max-w-md px-6 md:px-10 py-4 md:py-8 bg-dark-card shadow-inner shadow-neutral-800 rounded-lg md:rounded-xl">
-          <h3 className="text-2xl md:text-4xl font-bold text-center uppercase">Login</h3>
+        <div className="w-11/12 md:max-w-md px-6 md:px-10 py-6 md:py-8 bg-dark-card shadow-inner shadow-neutral-800 rounded-lg md:rounded-xl">
+          <h3 className="text-2xl md:text-4xl font-bold text-center uppercase mb-2 md:mb-4">
+            Login
+          </h3>
 
           <div className="flex w-full flex-col gap-6">
             <Input label="NIM" type="text" placeholder="240xxxxxxxxxxx" />
-            <Input label="Password" value={password} type={ passwordVisible ? 'text' : 'password' } placeholder="********" icon={ passwordVisible ? <i className="fa-solid fa-eye-slash"></i> : <i className="fa-solid fa-eye"></i>} onChange={(e) => setPassword(e.target.value)} onClickIcon={handlePasswordVisibility} />
+            <Input
+              label="Password"
+              value={password}
+              type={passwordVisible ? "text" : "password"}
+              placeholder="********"
+              icon={
+                passwordVisible ? (
+                  <i className="fa-solid fa-eye-slash"></i>
+                ) : (
+                  <i className="fa-solid fa-eye"></i>
+                )
+              }
+              onChange={(e) => setPassword(e.target.value)}
+              onClickIcon={handlePasswordVisibility}
+            />
           </div>
-  
-          <div className="flex justify-center mt-8">
-            <button className="bg-clip-border bg-neutral-950 px-10 py-3 rounded-md shadow-inner">
-              Login
-            </button>
+
+          <div className="flex justify-center mt-4 md:mt-8">
+            <Button label="Login" customClass="px-8 md:px-10 py-2 md:py-3" />
           </div>
         </div>
       </div>
