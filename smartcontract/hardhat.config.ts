62cf7b9a46a07fd5585a214d559d4c27dc364346
ratriaseza <satriareza2002@gmail.com>
2024-08-23 01:17:49 +0700

    refactor: Add deployed addresses and debug artifacts for Election module

--- smartcontract/hardhat.config.ts
+++ smartcontract/hardhat.config.ts
@@ -1,8 +1,16 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+
+const ALCHEMY_API_KEY = vars.get("ALCHEMY_API_KEY");
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`0x${vars.get("SEPOLIA_ACCOUNT1")}`],
+    }
+  }
 };
 
 export default config;
