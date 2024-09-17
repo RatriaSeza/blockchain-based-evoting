4cbe21330261ddb60d7c12b94d184e9941f0bd1f
ratriaseza <satriareza2002@gmail.com>
2024-09-18 01:21:35 +0700

    refactor: Update Masters page in Admin dashboard

--- client/src/pages/Admin/masters/Masters.tsx
+++ client/src/pages/Admin/masters/Masters.tsx
@@ -1,11 +1,15 @@
 import { Footer } from "@components/Admin/ui/footer/Footer"
 import { Header } from "@components/Admin/ui/header/Header"
 import { Sidebar } from "@components/Admin/ui/sidebar/Sidebar"
+import { ChevronDownIcon, ChevronRightIcon } from "@heroicons/react/24/solid"
+import { useState } from "react"
 
 export const Masters = () => {
+  const [active, setActive] = useState("");
+
   return (
     <main className="bg-surface">
-      <div id="main-wrapper" className="flex p-5 xl:pr-0">
+      <div id="main-wrapper" className="flex p-5 xl:pr-0 h-dvh">
         <aside
           id="application-sidebar-brand"
           className="hs-overlay hs-overlay-open:translate-x-0 -translate-x-full transform hidden xl:block xl:translate-x-0 xl:end-auto xl:bottom-0 fixed xl:top-5 xl:left-auto top-0 left-0 with-vertical h-screen z-[999] shrink-0 w-[270px] shadow-md xl:rounded-md rounded-none bg-white left-sidebar transition-all duration-300"
@@ -20,6 +24,38 @@ export const Masters = () => {
               </header>
 
               {/* main content */}
+              <div className="card">
+                <div className="card-body">
+									<h6 className="text-lg text-gray-500 font-semibold mb-6">Masters</h6>
+                  
+                  <button 
+                    onClick={() => setActive(active === 'candidates' ? "" : 'candidates')}
+                    className="flex justify-between w-full px-4 py-2 text-neutral-500 border rounded-lg shadow mb-2 cursor-pointer hover:bg-gray-100 active:bg-gray-50">
+                    <p className="font-medium">Candidates</p>
+                    <span>
+                      {active === 'candidates' ? (
+                        <ChevronDownIcon className="size-6" />
+                      ) : (
+                        <ChevronRightIcon className="size-6" /> 
+                      )}
+                    </span>
+                  </button>
+                  
+                  <button 
+                    onClick={() => setActive(active === 'deadline' ? "" : 'deadline')}
+                    className="flex justify-between w-full px-4 py-2 text-neutral-500 border rounded-lg shadow mb-2 cursor-pointer hover:bg-gray-100 active:bg-gray-50">
+                    <p className="font-medium">Deadline</p>
+                    <span>
+                      {active === 'deadline' ? (
+                        <ChevronDownIcon className="size-6" />
+                      ) : (
+                        <ChevronRightIcon className="size-6" /> 
+                      )}
+                    </span>
+                  </button>
+                </div>
+              </div>
+
               <Footer />
             </div>
           </main>
