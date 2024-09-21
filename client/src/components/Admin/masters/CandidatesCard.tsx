6fe4aaeb715b14a3498a512a6377905899c9b53b
ratriaseza <satriareza2002@gmail.com>
2024-09-22 01:23:39 +0700

    refactor: Add CandidatesCard component to Admin dashboard and implement modal functionality

--- client/src/components/Admin/masters/CandidatesCard.tsx
+++ client/src/components/Admin/masters/CandidatesCard.tsx
@@ -1,6 +1,8 @@
 import { XMarkIcon, PlusIcon } from "@heroicons/react/24/solid";
-import React from "react";
+import React, { useState } from "react";
 import { CandidatesTable } from "./CandidatesTable";
+
+import { Dialog, DialogHeader, DialogFooter, DialogBody, Button } from "@material-tailwind/react";
 
 type CandidatesCardProps = {
   onCloseClick: () => void;
@@ -9,6 +11,8 @@ type CandidatesCardProps = {
 export const CandidatesCard: React.FC<CandidatesCardProps> = ({
   onCloseClick,
 }) => {
+  const [openAddModal, setOpenAddModal] = useState(false);
+
   return (
     <div>
       <div className="flex justify-between items-center p-6">
@@ -22,13 +26,38 @@ export const CandidatesCard: React.FC<CandidatesCardProps> = ({
       </div>
 
       <div className="flex justify-end mx-6">
-        <button className="flex items-center gap-1 px-4 py-2 text-neutral-500 text-sm font-medium border rounded-lg shadow mb-2 cursor-pointer hover:bg-gray-100 active:bg-gray-50">
+        <button 
+          onClick={() => setOpenAddModal(!openAddModal)}
+          className="flex items-center gap-1 px-4 py-2 text-neutral-500 text-sm font-medium border rounded-lg shadow mb-2 cursor-pointer hover:bg-gray-100 active:bg-gray-50">
           <PlusIcon className="size-4" />
           <p>Add</p>
         </button>
       </div>
 
       <CandidatesTable />
+
+      <Dialog open={openAddModal}>
+        <DialogHeader>Its a simple modal.</DialogHeader>
+        <DialogBody>
+          The key to more success is to have a lot of pillows. Put it this way,
+          it took me twenty five years to get these plants, twenty five years of
+          blood sweat and tears, and I&apos;m never giving up, I&apos;m just
+          getting started. I&apos;m up to something. Fan luv.
+        </DialogBody>
+        <DialogFooter>
+          <Button
+            variant="text"
+            color="red"
+            onClick={() => setOpenAddModal(!openAddModal)}
+            className="mr-1"
+          >
+            <span>Cancel</span>
+          </Button>
+          <Button variant="gradient" color="green" onClick={() => setOpenAddModal(!openAddModal)}>
+            <span>Confirm</span>
+          </Button>
+        </DialogFooter>
+      </Dialog>
     </div>
   );
 };
