514445192a03bbd887017a4ed5604e0747018a1d
RatriaSeza <satriareza2002@gmail.com>
2024-11-05 17:34:49 +0700

    feat: add deleteById endpoint for Master and update routes

--- server/src/controllers/MasterController.ts
+++ server/src/controllers/MasterController.ts
@@ -1,5 +1,6 @@
 import { Response, Request } from "express";
 import { Master } from "../models/Master";
+import mongoose from "mongoose";
 
 export const getMasters = async (req: Request, res: Response): Promise<void> => {
   try {
@@ -32,3 +33,24 @@ export const createMaster = async (req: Request, res: Response): Promise<void> =
     res.status(500).json({ error: error.message });
   }
 };
+
+export const deleteById = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      res.status(400).json({ error: "Invalid candidate ID" });
+      return;
+    }
+
+    const deleteMaster = await Master.findByIdAndDelete(id);
+    if (!deleteMaster) {
+      res.status(404).json({ message: "Master not found" });
+      return;
+    }
+
+    res.status(200).json({ message: "Master deleted successfully", master: deleteMaster });
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
+}
--- server/src/routes/MasterRoutes.ts
+++ server/src/routes/MasterRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getMasters, getMasterByKey, createMaster } from "../controllers/MasterController";
+import { getMasters, getMasterByKey, createMaster, deleteById } from "../controllers/MasterController";
 
 const router = Router();
 
 router.get("/masters", getMasters);
 router.get("/masters/:key", getMasterByKey);
 router.post("/masters", createMaster);
+router.delete("/masters/:id", deleteById);
 
 export default router;
